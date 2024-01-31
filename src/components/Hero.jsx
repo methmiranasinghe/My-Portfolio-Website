45e2c203fa86107b6b06839f366579a1c072bb3c
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-01-31 19:21:08 +0530

    Added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+//import {motion} from "framer-motion";
+import {styles} from "../styles"
+import {ComputersCanvas} from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='realtive w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi &nbsp; I 'm  &nbsp;<span className='text-[#925eff]'>  Methmi</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}> I am a diligent individual with a strong enthusiasm for continuous learning. 
+          My expertise lies in the development of both mobile and web applications and I design user interfaces. 
+         </p>
+        </div>
+       
+      </div>
+     <ComputersCanvas/>
+      
+    </section>
   )
 }
 
