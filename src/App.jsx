23fdecc81a11a476252a72eaeccc0f15da9899f5
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-02-01 20:04:54 +0530

    Fixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-
+import React from "react";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
@@ -8,9 +8,7 @@ const App = () => {
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
-          
           <Hero />
-        
         </div>
         <About />
         <Experience />
